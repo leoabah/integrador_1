ca956d2828648b7f24f5b2057854977558a0d529
leoabah <leoabah@gmail.com>
2026-02-08 22:29:09 -0300

    actualizacion 1.4

--- scripts.js
+++ scripts.js
@@ -4,76 +4,97 @@ burger.addEventListener('click', () => {
     burgerCont.classList.toggle('active');
 });
 
-// const silder=document.getElementById("imgContainer");
-// const previor= document.getElementById("Prev");
-// const next = document.getElementById("Next");
-
-// next.addEventListener("click", (next)=>{
-//     imgContainer.scrollBy({left:auto,behavior: smooth})
-//   });
-// previor.addEventListener("click",(previor)=>{
-//     imgContainer.scrollBy({left: auto, behavior: smooth })
-// }) ;
 
 
 
-newFunction();
+
+
 
 function newFunction() {
-    const formAltas = document.getElementById("formAltas");
+    const form = document.getElementById("formAltas");
 
     const inputName = document.getElementById("userName");
     const inputPrice = document.getElementById("Price");
     const inputAge = document.getElementById("Age");
     const inputStock = document.getElementById("Stock");
     const inputFoto = document.getElementById("Foto");
-    // const inputSdecript = document.getElementById("ShrotDescrp");
-    // const inputLdecript = document.getElementById("longDescrp");
-    const texto = document.getElementById("texto");
 
-    const regexName = /^[a-zA-Z]{3,}$/;
+    const Error = document.getElementById("ErrorMensaje");
+
+    const regexName = /^[a-zA-Z\s]{3,}$/;
     const regexPrice = /^\d+(\.\d{1,2})?$/;
     const regexAge = /^\d+$/;
     const regexStock = /^\d+$/;
-    const Archivo = inputFoto.File[0];
+    const archivo = inputFoto.File[0];
 
 
-    formAltas.addEventListener("submit", (event) => {
+    form.addEventListener("submit", (event) => {
         event.preventDefault();
 
-        if (!regexName.Text(inputName.Value)) {
-            texto.textContent = "Error: nombre invalido";
+        if (!regexName.test(inputName.Value)) {
+            Error.textContent = "Error: nombre invalido(Minimo 3 caracteres)";
             return;
         }
 
-        if (!regexPrice.Text(inputPrice.Value)) {
-            texto.textContent = "Error precio invalidado";
+        if (!regexPrice.test(inputPrice.Value)) {
+            Error.textContent = "Error precio invalidado";
             return;
         }
 
         if (!regexAge.test(inputAge.Value)) {
-            texto.textContent.textContent = "Error edad invalida";
+            Error.textContent = "Error edad invalida";
             return;
         }
         if (!regexStock.test(inputStock.Value)) {
-            texto.textContent = "Error  stock invalidado";
+            Error.textContent = "Error  stock invalidado";
             return;
         }
         if (inputFoto.Files.length === 0) {
-            texto.textContent = "Error: debes eligir una imagen invalidado";
+            Error.textContent = "Error: eligir una imagen invalidado";
             return;
         }
-        if (!Archivo.type.startsWith("images/")) {
-            texto.textContent = "Error invalidado";
+        if (!archivo.type.startsWith("image/")) {
+            Error.textContent = "Error invalidado";
             return;
         }
 
-        texto.textContent = "Formulario enviado correctamente ✔";
+        Error.textContent = "";
+        alert("Formulario enviado correctamente ✔")
+        form.reset();
 
     });
-
-
-
-    const input = document.getElementById("emAil");
 }
-// const regexEmail =  ;
+
+newFunction();
+ 
+
+function contFunction() { 
+    
+    const fromingres = document.getElementById("formIngres");
+    const inputName = document.getElementById("Username")
+    const inputEmail = document.getElementById("Email")
+    
+    const Error = document.getElementById("ErrorMensaje")
+    
+    const regexName = /^[a-zA-Z]{3,}$/;
+    const regexMail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+
+
+fromingres.addEventListener("submit",(event) => {
+    event.preventDefault();
+
+
+    if (!regexName.test(inputName.Value)) {
+        Error=textContent("Error: nombre invalido");
+        return;
+    }
+    if (!regexMail.test(inputEmail.Value)) {        texto= textContent(" Error: email invalio");
+        return;
+    }
+   })
+
+   Error = textContent("Enviado con existo") ;
+}
+
+contFunction();
